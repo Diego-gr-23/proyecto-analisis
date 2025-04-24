c396ecf7e61ae174c01d283c270041740266a866
Diego Gómez <luisdiegojose23@gmail.com>
2025-04-23 18:31:44 -0600

    agregue el area para vendedores y hice unas modificaciones

--- phytovierPharma_/src/login.tsx
+++ phytovierPharma_/src/login.tsx
@@ -1,6 +1,6 @@
 // src/Login.tsx
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'  // Importar el hook useNavigate
+import { useNavigate } from 'react-router-dom'
 import './login.css'
 
 const Login: React.FC = () => {
@@ -10,11 +10,13 @@ const Login: React.FC = () => {
 
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault()
-    
-    // Aquí puedes agregar tu lógica de validación (por ejemplo, autenticación)
+
     if (username === 'admin' && password === 'password') {
-      // Si la validación es exitosa, navega a la página de Inicio
-      navigate('/inicio') // Redirige a la ruta /inicio
+      localStorage.setItem('userRole', 'admin')
+      navigate('/inicio') // Redirige a la vista de admin
+    } else if (username === 'vendedor' && password === '1234') {
+      localStorage.setItem('userRole', 'vendedor')
+      navigate('/inicio') // Redirige a la vista de vendedor
     } else {
       alert('Usuario o contraseña incorrectos.')
     }
@@ -33,7 +35,7 @@ const Login: React.FC = () => {
           onChange={(e) => setUsername(e.target.value)}
           required
         />
-        
+
         <label htmlFor="password">Contraseña:</label>
         <input
           type="password"
@@ -43,7 +45,7 @@ const Login: React.FC = () => {
           onChange={(e) => setPassword(e.target.value)}
           required
         />
-        
+
         <button type="submit">Ingresar</button>
       </form>
     </div>
@@ -51,4 +53,59 @@ const Login: React.FC = () => {
 }
 
 export default Login
+
+
+// // src/Login.tsx
+// import React, { useState } from 'react'
+// import { useNavigate } from 'react-router-dom'  // Importar el hook useNavigate
+// import './login.css'
+
+// const Login: React.FC = () => {
+//   const [username, setUsername] = useState('')
+//   const [password, setPassword] = useState('')
+//   const navigate = useNavigate()
+
+//   const handleSubmit = (event: React.FormEvent) => {
+//     event.preventDefault()
+    
+//     // Aquí puedes agregar tu lógica de validación (por ejemplo, autenticación)
+//     if (username === 'admin' && password === 'password') {
+//       // Si la validación es exitosa, navega a la página de Inicio
+//       navigate('/inicio') // Redirige a la ruta /inicio
+//     } else {
+//       alert('Usuario o contraseña incorrectos.')
+//     }
+//   }
+
+//   return (
+//     <div className="login-container">
+//       <h2>Iniciar Sesión</h2>
+//       <form onSubmit={handleSubmit}>
+//         <label htmlFor="username">Usuario:</label>
+//         <input
+//           type="text"
+//           id="username"
+//           name="username"
+//           value={username}
+//           onChange={(e) => setUsername(e.target.value)}
+//           required
+//         />
+        
+//         <label htmlFor="password">Contraseña:</label>
+//         <input
+//           type="password"
+//           id="password"
+//           name="password"
+//           value={password}
+//           onChange={(e) => setPassword(e.target.value)}
+//           required
+//         />
+        
+//         <button type="submit">Ingresar</button>
+//       </form>
+//     </div>
+//   )
+// }
+
+// export default Login
 
